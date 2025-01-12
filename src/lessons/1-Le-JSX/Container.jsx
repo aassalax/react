982fc6a3a50958619e447abf2603eeac95f46179
aassalax <aassali.dev@gmail.com>
2025-01-12 15:11:24 +0100

    event listener : anonymous function

--- src/lessons/1-Le-JSX/Container.jsx
+++ src/lessons/1-Le-JSX/Container.jsx
@@ -1,8 +1,11 @@
+import './Container.css'
+
 export default function Container(){
     const txt = 'Hello world from container'
     return(
         <>
             <p>{txt}</p>
+            <button onClick={() => console.log("click!")}>Click me !</button>
         </>
     )
 }
