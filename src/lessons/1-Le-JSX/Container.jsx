58e035274aa063937303a850769d599760b1d61e
aassalax <aassali.dev@gmail.com>
2025-01-12 15:20:54 +0100

    event listener : with function

--- src/lessons/1-Le-JSX/Container.jsx
+++ src/lessons/1-Le-JSX/Container.jsx
@@ -2,10 +2,20 @@ import './Container.css'
 
 export default function Container(){
     const txt = 'Hello world from container'
+    function handleClick(){
+        console.log("click!!")
+    }
+
     return(
         <>
             <p>{txt}</p>
-            <button onClick={() => console.log("click!")}>Click me !</button>
+            <button onClick={handleClick}>Click me !</button>
         </>
     )
 }
+
+/*
+* onClick={() => console.log("click!")}
+* OKAY si juste une ligne
+* sinon préféré la création d'une function
+* */
