b6e4cc765968b97cf9b1cefdaf6ef5d3d7966231
aassalax <aassali.dev@gmail.com>
2025-01-15 14:20:00 +0100

    add item with validation

--- 2-Project-TodoList/src/App.jsx
+++ 2-Project-TodoList/src/App.jsx
@@ -11,26 +11,48 @@ function App() {
         {id: nanoid(8), content: "Un test"},
     ])
 
-    function deleteTodo(id){
+    const [todo, setTodo] = useState("");
+    const [showValidation, setShowValidation] = useState(false);
+
+    function deleteTodo(id) {
         setTodoList(todoList.filter(item => item.id !== id))
     }
+    function handleSubmit(e){
+        e.preventDefault()
+        if(todo === ""){
+            setShowValidation(true)
+            return
+        }
+        setShowValidation(false)
+        setTodoList([...todoList,{id:nanoid(8), content: todo}])
+        setTodo("")
+    }
+
+    function todoListHasItem() {return todoList.length > 0}
+    function todoListIsEmpty() {return !todoListHasItem()}
 
     return (
-        <div className="h-screen bg-slate-900">
+        <div className="h-screen">
             <div className="max-w-4xl mx-auto pt-20 px-6">
                 <h1 className="text-3xl text-slate-100 mb-4">La To-Do List</h1>
-                <form className="mb-10">
+                <form onSubmit={handleSubmit} className="mb-10">
                     <label htmlFor="todo-item" className="text-slate-50">Ajouter une chose à faire</label>
-                    <input type="text" className="mt-1 block w-full rounded"/>
+                    <input value={todo} onChange={e => setTodo(e.target.value)}
+                        type="text" className="mt-1 block w-full rounded"
+                    />
+                    {showValidation && <p className="text-red-600">Ajoutez du contenu à votre tâche</p>}
                     <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
                 </form>
-
-                <ul>
-                    {todoList.map(item => (
-                            <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo}/>
-                        )
-                    )}
-                </ul>
+                {todoListIsEmpty() && <p className="text-slate-50">Pas d'items à afficher...</p>}
+                {todoListHasItem() &&
+                    <ul>
+                        {todoList.map(item => <ListItem
+                            key={item.id}
+                            itemData={item}
+                            deleteTodo={deleteTodo}/>
+                        )}
+                    </ul>
+                }
             </div>
         </div>
     )
