5e1d0e44aa4d359beae14893551e1b0a675bc104
aassalax <aassali.dev@gmail.com>
2025-01-15 19:43:42 +0100

    lexique

--- 3-React-Dans-Le-Detail/src/lessons/3-Le-Hook-use-effect/Container.jsx
+++ 3-React-Dans-Le-Detail/src/lessons/3-Le-Hook-use-effect/Container.jsx
@@ -16,6 +16,17 @@ export default function Container(){
         console.log("After first creation & after each maj of count");
     }, [count]); //if deps pass, effect will be trigger on init and maj of deps
 
+    //Lexique :
+    // Render => exectution of a componant (l.3 see "function")
+    // The first render of a component is call : the Mount
+    // The nth render is often call re-render => MAJ the Virtual-DOM
+    // Virtual DOM => Is manage by React
+    // Commit => Step when passing from virtual DOM to (real) DOM
+    // When commit step :
+        // 1- Mount or Re-render (MAJ)
+        // 2- Execution of useEffect
+    // Unmount => Destruction of a component already present in the DOM
+
     console.log("MAJ count : ",count);
 
     return(
