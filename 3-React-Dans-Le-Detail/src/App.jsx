2a56ad1e3c1bcf331c433ad52c9cd9f0054bf0cb
aassalax <aassali.dev@gmail.com>
2025-01-17 22:55:43 +0100

    props children is props inside the jsx balise

--- 3-React-Dans-Le-Detail/src/App.jsx
+++ 3-React-Dans-Le-Detail/src/App.jsx
@@ -1,12 +1,22 @@
-import StaticRefSelection from "./lessons/9-Selectionner-un-tableau-d-elements/StaticRefSelection.jsx"
-import DynamicRefSelection from "./lessons/9-Selectionner-un-tableau-d-elements/DynamicRefSelection.jsx";
+import Child from "./lessons/10-Comprendre-PropsChildren/Child.jsx";
 
 
 function App() {
   return (
     <div className="main-content">
-        {false && <StaticRefSelection/>}
-        <DynamicRefSelection />
+        <Child>
+            <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eveniet ex nemo nisi suscipit! Ab aliquid autem beatae ea earum fuga, inventore necessitatibus, quam, qui quisquam recusandae sequi vero vitae! Ab asperiores atque dolore eius eligendi enim, eos facere incidunt, iste, laborum minima nesciunt nostrum nulla provident quasi ratione sed?</p>
+        </Child>
+        <Child txt={'test'}>
+            {/*here txt is not a props children*/}
+            <button>click me</button>
+        </Child>
+        <Child>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+        </Child>
     </div>
   )
 }
