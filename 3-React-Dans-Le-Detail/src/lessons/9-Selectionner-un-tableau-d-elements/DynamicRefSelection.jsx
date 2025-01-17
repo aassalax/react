38164c01041b5d3b473f559d7f0c89fd5a24d7fc
aassalax <aassali.dev@gmail.com>
2025-01-17 21:30:05 +0100

    Select dynamic element

--- 3-React-Dans-Le-Detail/src/lessons/9-Selectionner-un-tableau-d-elements/DynamicRefSelection.jsx
+++ 3-React-Dans-Le-Detail/src/lessons/9-Selectionner-un-tableau-d-elements/DynamicRefSelection.jsx
@@ -1,8 +1,51 @@
-export default function DynamicRefSelection(){
+import {useRef, useState} from "react";
+import {nanoid} from "nanoid";
 
-    return(
-        <>
+export default function DynamicRefSelection() {
+
+    const [fruits, setFruits] = useState([
+        {id: nanoid(8), value: 'Fraise'},
+        {id: nanoid(8), value: '🙃'},
+        {id: nanoid(8), value: '🥭'},
+    ])
+
+    function deleteFruit(fruitToDeleteId){
+        setFruits(fruits
+            .filter(fruit => fruit.id !== fruitToDeleteId)
+        )
+    }
+
+    function addFruit(){
+        setFruits([...fruits,{id:nanoid(8), value:'Tomato'}])
+    }
+
+    const fruitsRef = useRef([])
+
+    function handleRefs(element){
+        if(element) {
+            fruitsRef.current.push(element)
+        }
+        else {
+            fruitsRef.current.shift()
+        }
+        console.log(fruitsRef);
+    }
+
+    return (
+        <div>
             <h1>Selectionner des elements dynamiques</h1>
-        </>
+            <ul>
+                {fruits.map(fruit => (
+                        <li
+                            ref={handleRefs}
+                            onClick={() => deleteFruit(fruit.id)}
+                            key={fruit.id}>
+                            {fruit.value}
+                        </li>
+                    )
+                )}
+            </ul>
+            <button onClick={addFruit}>Ajouter une tomate</button>
+        </div>
     )
 }
