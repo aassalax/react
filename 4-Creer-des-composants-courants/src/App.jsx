c43c9d82cf46d00672d1cc2c505aa2a2b1b7e2d4
aassalax <aassali.dev@gmail.com>
2025-01-18 18:56:18 +0100

    Tabs

--- 4-Creer-des-composants-courants/src/App.jsx
+++ 4-Creer-des-composants-courants/src/App.jsx
@@ -1,9 +1,11 @@
 import ModalButton from "./lessons/1-Les-Modals/ModalButton";
+import Tabs from "./lessons/2-Les-Tabs/Tabs.jsx";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
       <ModalButton/>
+        <Tabs/>
     </div>
   )
 }
