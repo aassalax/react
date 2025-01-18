b9df08f67ecee1aae34a564a5f7e2453494a4083
aassalax <aassali.dev@gmail.com>
2025-01-18 19:29:03 +0100

    navbar

--- 4-Creer-des-composants-courants/src/App.jsx
+++ 4-Creer-des-composants-courants/src/App.jsx
@@ -1,10 +1,12 @@
 import ModalButton from "./lessons/1-Les-Modals/ModalButton";
 import Tabs from "./lessons/2-Les-Tabs/Tabs.jsx";
+import Navbar from "./lessons/3-La-navbar/Navbar.jsx";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-700 pt-10">
-      <ModalButton/>
+    <div className="min-h-screen bg-slate-700 pt-20">
+        <Navbar />
+        <ModalButton/>
         <Tabs/>
     </div>
   )
