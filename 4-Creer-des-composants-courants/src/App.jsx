2c4c52cf28130bf57db95ba8470fba148744f3f4
aassalax <aassali.dev@gmail.com>
2025-01-18 18:31:35 +0100

    Modal

--- 4-Creer-des-composants-courants/src/App.jsx
+++ 4-Creer-des-composants-courants/src/App.jsx
@@ -1,6 +1,9 @@
+import ModalButton from "./lessons/1-Les-Modals/ModalButton";
+
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
+      <ModalButton/>
     </div>
   )
 }
