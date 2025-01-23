63ab6e5e45a941c61dd6d9942f11258fbed41124
aassalax <aassali.dev@gmail.com>
2025-01-24 00:03:08 +0100

    Context Api is a native react state manager

--- 6-Context-API/src/components/Content/Content.jsx
+++ 6-Context-API/src/components/Content/Content.jsx
@@ -1,10 +1,14 @@
 import './Content.css'
+import {useContext} from "react";
+import {ThemeContext} from "../../context/ThemeProvider.jsx";
 
 export default function Content(){
 
+    const {darkMode} = useContext(ThemeContext)
+
     return(
         <div className="container">
-            <h1>Dark / Light app</h1>
+            <h1>You are in {darkMode ? 'Dark' : 'Light'} mode</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet aperiam atque aut consectetur debitis delectus deserunt eaque earum eligendi exercitationem expedita harum inventore maxime molestias mollitia, nesciunt nihil nobis numquam omnis praesentium, quisquam ratione recusandae tempore voluptate voluptatibus. A aspernatur ex impedit nisi perspiciatis! Deleniti ex fugiat in iure nemo odit quas rerum, temporibus? Aperiam commodi distinctio, doloribus explicabo fuga illo magnam necessitatibus, quasi quos similique, unde vel. Animi atque eaque ex inventore nostrum optio. At, blanditiis commodi cupiditate debitis doloremque dolorum enim fugit impedit maxime minus nam porro quidem repellat repellendus, rerum sit tempora unde ut voluptatum.</p>
         </div>
     )
--- 6-Context-API/src/components/ToggleThemeButton/ToggleThemeButton.jsx
+++ 6-Context-API/src/components/ToggleThemeButton/ToggleThemeButton.jsx
@@ -1,12 +1,15 @@
 import './ToggleThemeButton.css'
 import sun from '../../assets/react.svg'
+import {useContext} from "react";
+import {ThemeContext} from "../../context/ThemeProvider.jsx";
 
 export default function ToggleThemeButton(){
 
+    const {toggleTheme, darkMode} = useContext(ThemeContext)
 
     return(
-        <button className="toggle-theme-btn">
-            <span>Go Dark</span>
+        <button className="toggle-theme-btn" onClick={toggleTheme}>
+            <span>Go {darkMode ? 'Light' : 'Dark'}</span>
             <img src={sun} alt="toggle icone"/>
         </button>
     )
