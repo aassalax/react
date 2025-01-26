89f6f3ab2019fe0733b6e33572ed099614c58f93
aassalax <aassali.dev@gmail.com>
2025-01-26 16:40:29 +0100

    set up Fruity UI

--- 7-React-Redux/src/App.jsx
+++ 7-React-Redux/src/App.jsx
@@ -1,11 +1,14 @@
 import Counter from "./pages/Counter/Counter.jsx";
+import Fruity from "./pages/Fruity/Fruity.jsx";
 
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-700 px-4">
         <div className="mx-auto max-w-3xl pt-10 pb-20">
-            <Counter/>
+            {/*1- Counter app*/}
+            {/*<Counter/>*/}
+            <Fruity/>
         </div>
     </div>
   )
---
+++ 7-React-Redux/src/pages/Fruity/FruityCart.jsx
@@ -0,0 +1,12 @@
+export default function FruityCart(){
+
+    return(
+        <div className="bg-slate-100 rounded">
+            <p className="text-1xl border-b p-5 border-slate-400">Youir FruityCart</p>
+
+            <ul></ul>
+
+            <p className="text-xl p-5 border-t border-slate-400">Total price : 0$</p>
+        </div>
+    )
+}
