50149d06759bf7ea49a1d3dbd77b84944faf6f7a
aassalax <aassali.dev@gmail.com>
2025-01-26 18:24:58 +0100

    custom middleware

--- 7-React-Redux/src/store.js
+++ 7-React-Redux/src/store.js
@@ -2,12 +2,37 @@ import counter from "./features/counter.js";
 import {configureStore} from "@reduxjs/toolkit";
 import fruits from "./features/fruits.js";
 import fruitsCart from "./features/fruitsCart.js";
+import mango from "./assets/mango.jpeg";
 
 
 export const store = configureStore({
-    reducer : {
+    reducer: {
         counter,
         fruits,
         fruitsCart
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        /*customMiddleware*/
+    )
 })
+
+function customMiddleware(store) {
+    return function (next) {
+        return function (action) {
+            //console.log(store.getState());
+            //console.log(next);
+            //console.log(action);
+
+            //remplacer toute les actions par une autre :
+            /*next({
+                type: "fruitsCart/addOne",
+                payload: {
+                    name: "Mango",
+                    url: "mangoURL",
+                    price: 999,
+                    id: 15613186
+                }
+            })*/
+        }
+    }
+}
