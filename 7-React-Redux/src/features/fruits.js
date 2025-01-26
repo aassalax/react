1e5d995b708cba7fde8d2d168ac3c85a9734ca9d
aassalax <aassali.dev@gmail.com>
2025-01-26 18:13:55 +0100

    extra reducers

--- 7-React-Redux/src/features/fruits.js
+++ 7-React-Redux/src/features/fruits.js
@@ -22,7 +22,13 @@ const initialState = {
 
 export const fruits = createSlice({
     name: "fruits",
-    initialState
+    initialState,
+    reducers : {},
+    extraReducers : (builder) => {
+        builder.addCase("fruitsCart/addOne", (state, action) => {
+            console.log("Hello fruit reducer is triggered by an fruitsCart actions")
+        })
+    }
 })
 
 export default fruits.reducer
